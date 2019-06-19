4d03310940a64bc38dc8e08234c90806bfcfc7f4
Alejandro Mogollon Medina <935@holbertonschool.com>
2019-06-19 20:33:00 +0000

    Now this should work fine, taking into account the Betty linter

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
-#include<stdio.h> 
-int main() 
-{ 
-  puts("\"Programming is like building a multilingual puzzle"); 
-      
-  getchar(); 
-  return 0; 
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: A string
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
