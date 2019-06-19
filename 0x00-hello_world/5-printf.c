7fc76cf088e2dfd98f697f2c20616d6346aa83bf
Alejandro Mogollon Medina <935@holbertonschool.com>
2019-06-19 20:36:11 +0000

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: A text
+ */
+
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
