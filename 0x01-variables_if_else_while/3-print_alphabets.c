e7d57fe031be486353f84c0068dc2c12b026dfac
Alejandro Mogollon Medina <935@holbertonschool.com>
2019-06-20 16:09:23 -0500

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: The alphabet in lowercase
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+		putchar(c);
+
+	for (c = 'A' ; c <= 'Z' ; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
