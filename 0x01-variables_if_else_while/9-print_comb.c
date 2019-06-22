3050be3ffe787351ac016131b471ff4fe8042cd9
Alejandro Mogollon Medina <935@holbertonschool.com>
2019-06-22 00:54:59 +0000

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: All single digit numbers of base 10 starting from 0
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = '0' ; c <= '9' ; c++)
+	{
+		putchar(c);
+	if (c <= '8')
+       	putchar(',');
+	putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
