00cdf51b92536c3a9859bb904c0dbd8bebf62760
Alejandro Mogollon Medina <935@holbertonschool.com>
2019-06-25 01:03:57 +0000

    islower

---
+++ 0x02-functions-low-level_programming/3-islower.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * int _islower - entry point
+ *
+ * Description: Checks for lowercase character through
+ *
+ * Return: 1 if c is lowercase, returns 0 otherwise
+ */
+
+/* int _islower prototype function */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
