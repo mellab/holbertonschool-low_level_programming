fc48eebb496b37649062b4ca093ec6fd8e50daaf
Alejandro Mogollon Medina <935@holbertonschool.com>
2019-06-26 04:32:38 +0000

    Create the 0x03-debugging directory with its respective readme file and the holberton.h header file

---
+++ 0x03-debugging/holberton.h
@@ -0,0 +1,14 @@
+#ifndef _holberton_h_
+#define _holberton_h_
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+
+int largest_number(int a, int b, int c);
+
+void print_remaining_days(int month, int day, int year);
+
+int convert_day(int month, int day);
+
+#endif /* _holberton_h_ */
