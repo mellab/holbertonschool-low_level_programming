ccfe89f0616d4e2cbc26a67708886f708c14e39d
Alejandro Mogollon Medina <935@holbertonschool.com>
2019-06-27 13:20:39 -0500

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ *
+ *
+ *
+ */
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0' ; c <= '9' ; c++)
+		_putchar(c);
+	_putchar ('\n');
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,10 +1,14 @@
 #ifndef _holberton_h_
 #define _holberton_h_
 
+int _putchar(char c);
+
 int _isupper(int c);
 
 int _isdigit(int c);
 
 int mul(int a, int b);
+
+void print_numbers(void);
 
 #endif /* _holberton_h_ */
