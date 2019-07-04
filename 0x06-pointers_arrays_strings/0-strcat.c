b9b6d09266ab1f87e3754669e252eac1b5b606af
Alejandro Mogollon Medina <935@holbertonschool.com>
2019-07-04 10:37:57 -0500

    Create the 0x06-pointers_arrays_strings directory with its respective readme file and the holberton.h header file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * *_strcat - concatenates two strings
+ * @dest: string number one
+ * @src: string number two
+ *
+ * Return: a concatenated string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	for (dest[i] != '\0')
+	{
+		++i;
+	}
+
+	for (src[j] != '\0')
+	{
+		++j;
+		++i;
+		dest[i] = src[j];
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,4 @@
+#ifndef _holberton_h_
+#define _holberton_h_
+
+#endif /* _holberton_h_ */
