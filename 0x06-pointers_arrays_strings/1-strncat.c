49076aba3e8d60b03cc99854aba9d5ab6996bb61
Alejandro Mogollon Medina <935@holbertonschool.com>
2019-07-05 03:37:53 +0000

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * *_strncat - concatenates two strings
+ * @dest: string number one
+ * @src: string number two
+ *
+ * Return: a concatenated string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; ++i)
+		;
+
+	for (j = 0; src[j] != '\0' && i < 97 && j < n; ++j, ++i)
+	{
+		dest[i] = src[j];
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
