f2441b609c51dd0047ac99f97492c638e9654fc6
Alejandro Mogollon Medina <935@holbertonschool.com>
2019-07-08 11:26:40 -0500

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: string
+ * @b: constant byte
+ * @n: length of string
+ *
+ * Return: a pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n > 0)
+	{
+		*s = b;
+		s++, n--;
+	}
+	return (0);
+}
--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,4 +1,6 @@
 #ifndef _holberton_h_
 #define _holberton_h_
 
+char *_memset(char *s, char b, unsigned int n);
+
 #endif /* _holberton_h_ */
