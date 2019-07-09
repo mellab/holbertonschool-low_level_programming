94e86b6467a2bd16be56890f23ac27cb7685066f
Alejandro Mogollon Medina <935@holbertonschool.com>
2019-07-09 04:05:42 +0000

    Double pointer, double fun

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * set_string - sets the value of a pointer to a char.
+ * @s: first string
+ * @to: second string
+ *
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -19,4 +19,6 @@ void print_chessboard(char (*a)[8]);
 
 void print_diagsums(int *a, int size);
 
+void set_string(char **s, char *to);
+
 #endif /* _holberton_h_ */
