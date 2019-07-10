833ea40318b8fb57d53e8663b23c74b84832e1c6
Alejandro Mogollon Medina <935@holbertonschool.com>
2019-07-10 16:00:57 -0500

    I don't know what the hell I was thinking

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,12 +9,16 @@
 
 int factorial(int n)
 {
-	if (n >= 1)
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n >= 1)
 	{
 		return (n * factorial(n - 1));
 	}
-		else
-		{
-			return (1);
-		}
+	else
+	{
+		return (1);
+	}
 }
