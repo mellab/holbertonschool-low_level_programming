a05feb7b57ad513b27ce99dd499a770e62799df3
Alejandro Mogollon Medina <935@holbertonschool.com>
2019-07-10 13:35:14 -0500

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forge

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: string of characters to be printed
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -3,4 +3,6 @@
 
 int _putchar(char c);
 
+void _puts_recursion(char *s);
+
 #endif /* _holberton_h_ */
