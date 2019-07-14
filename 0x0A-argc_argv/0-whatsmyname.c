d39c2e4171f149f7bf218ccba8434b6036464431
Alejandro Mogollon Medina <935@holbertonschool.com>
2019-07-14 06:06:58 +0000

    Committing in accordance with the prophecy

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 /**
+ * main - prints its name, followed by a new line
+ * @argc: argument counter
+ * @argv: argument array
  *
- *
- *
- *
+ * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	(void)argc;
-	       return (0);
+	return (0);
 }
