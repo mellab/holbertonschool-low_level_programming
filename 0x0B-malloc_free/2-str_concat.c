04060ba3ad909e8bab9bb1ff3801430aa3a20ce7
Alejandro Mogollon Medina <935@holbertonschool.com>
2019-07-16 15:21:29 -0500

    My bad

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -25,14 +25,15 @@ char *str_concat(char *s1, char *s2)
 	p = malloc((a + b + 1) * sizeof(char));
 	if (p == NULL)
 		return (NULL);
-
 	for (c = 0; s1 && s1[c]; c++)
+	{
 		p[c] = s1[c];
-        p[c] = '\0';
-
+		p[c] = '\0';
+	}
 	for (c = 0; s2 && s2[c]; c++)
+	{
 		p[a + c] = s2[c];
 	p[a + c] = '\0';
-
+	}
 	return (p);
 }
