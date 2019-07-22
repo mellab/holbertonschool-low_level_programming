2a0145e6d982812fc94f74b817e1e9dd32c45595
Alejandro Mogollon Medina <935@holbertonschool.com>
2019-07-22 10:33:14 -0500

    Django

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _dog_h_
+#define _dog_h_
+
+/**
+ * struct dog - such structure, wow
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+#endif /* _dog_h_ */
