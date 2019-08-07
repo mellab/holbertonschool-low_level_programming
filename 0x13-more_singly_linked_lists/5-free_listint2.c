f8414d403533474556ad72735c18e59f1990def5
Alejandro Mogollon Medina <935@holbertonschool.com>
2019-08-07 04:05:04 +0000

    Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: pointer to the first node
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp, *currentNode;
+
+	if (head != NULL)
+	{
+		currentNode = *head;
+		while ((tmp = currentNode) != NULL)
+		{
+			currentNode = currentNode->next;
+			free(tmp);
+		}
+		*head = NULL;
+	}
+}
